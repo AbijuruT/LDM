351368212927038933374ce855890bda130cc061
Abijuru <abijuruyves@gmail.com>
2021-01-08 17:17:51 +0200

    Updated

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,3 +11,49 @@ const showMenu = (toggleId, navId) => {
 }
 showMenu('header-toggle', 'nav-menu')
 
+/*==== HOME SLIDESHOW MANUAL =====*/
+// var slideIndex = 1;
+// showSlides(slideIndex);
+
+// function plusSlides(n) {
+    //   showSlides((slideIndex += n));
+// }
+
+// function currentSlide(n) {
+//   showSlides((slideIndex = n));
+// }
+
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("my__slides");
+//   var dots = document.getElementsByClassName("indicator");
+//   if (n > slides.length) {
+    //     slideIndex = 1;
+//   }
+//   if (n < 1) {
+    //     slideIndex = slides.length;
+    //   }
+    //   for (i = 0; i < slides.length; i++) {
+        //     slides[i].style.display = "none";
+        //   }
+        //   for (i = 0; i < dots.length; i++) {
+            //     dots[i].className = dots[i].className.replace(" current", "");
+            //   }
+            //   slides[slideIndex - 1].style.display = "block";
+            //   dots[slideIndex - 1].className += " current";
+            // }
+/*==== HOME SLIDESHOW AUTO =====*/
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("my__slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 10000); // Change SLIDE every 10 seconds
+}
